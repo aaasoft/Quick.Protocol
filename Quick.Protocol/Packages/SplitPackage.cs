85c2b918d5d16710794078bd22c50b99dcfdb8f3
zhangpeng <scbeta@qq.com>
2020-07-15 16:20:26 +0800

    添加漏掉的文件。

---
+++ Quick.Protocol/Packages/SplitPackage.cs
@@ -0,0 +1,14 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quick.Protocol.Packages
+{
+    [ProtoContract]
+    public class SplitPackage : AbstractPackage
+    {
+        public const byte PACKAGE_TYPE = 2;
+        public override byte PackageType => PACKAGE_TYPE;
+    }
+}
